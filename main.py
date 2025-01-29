af0715f66bf200b9c5a96d91d8a742f501d36b5c
Adisa Malik <adisamalikng0@gmail.com>
2025-01-29 10:23:31 +0100

    Desired response received

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from datetime import datetime
+from datetime import datetime, timezone
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -13,6 +13,14 @@ app.add_middleware(
 
 @app.get("/")
 def getinfop():
+
+    current_time = datetime.now(timezone.utc)
+    formatted_time = current_time.isoformat(timespec="seconds").replace("+00:00", 'Z')
+
     data = {
-        "email"
+        "email": "Adisamalikng0@gmail.com",
+        "current_datetime": formatted_time,
+        "github_url": "https://github.com/Malik001-G/Stage-Zero-Public_Api"
     }
+
+    return data
